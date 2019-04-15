8be78d9d60af31ab9399a2548706c093cc56dde8
hushouguo <kilimajaro@gmail.com>
2019-04-15 14:34:17 +0800

    fix bug

---
+++ tnode/src/db/EasydbParser.cpp
@@ -0,0 +1,7 @@
+/*
+ * \file: EasydbParser.cpp
+ * \brief: Created by hushouguo at 14:34:00 Apr 15 2019
+ */
+
+#include "tnode.h"
+
---
+++ tnode/src/db/EasydbParser.h
@@ -0,0 +1,9 @@
+/*
+ * \file: EasydbParser.h
+ * \brief: Created by hushouguo at 14:33:58 Apr 15 2019
+ */
+ 
+#ifndef __EASYDBPARSER_H__
+#define __EASYDBPARSER_H__
+ 
+#endif
