7fd995073fca6467b64d898fd8d4b5cab1a7b9f0
hushouguo <kilimajaro@gmail.com>
2019-04-15 14:25:12 +0800

    fix bug

---
+++ tnode/src/db/mysql/MySQL.h
@@ -0,0 +1,54 @@
+/*
+ * \file: MySQL.h
+ * \brief: Created by hushouguo at 17:27:53 Apr 11 2019
+ */
+ 
+#ifndef __MYSQL_H__
+#define __MYSQL_H__
+
+namespace db {
+	struct MySQLConfig {
+		std::string host;
+		std::string user;
+		std::string passwd;
+		std::string database;
+		int port = -1;
+		bool parsefrom(std::string conf);	// host:port:user:password:db
+	};
+
+	class MySQLResult;	
+	class MySQL {
+		public:
+			MySQL();
+			~MySQL();
+
+		public:
+			bool openDatabase();
+			bool openDatabase(const char *host, const char *user, const char *passwd, const char *db, int port = 3306);
+			void closeDatabase();
+
+		public:
+			bool runCommand(std::string s);
+			MySQLResult* runQuery(std::string s);
+
+		public:
+			bool resetDatabase();
+			bool selectDatabase(std::string database);
+			bool createDatabase(std::string database);
+			bool deleteDatabase(std::string database);
+			bool findDatabase(std::string database);
+			bool loadDatabase(std::string where, std::set<std::string>& results);
+			bool loadTable(std::string where, std::set<std::string>& results);
+
+		public:
+			u64 insertId();
+
+		private:
+			friend class MySQLStatement;
+			MYSQL _mysqlhandle;
+			MySQLConfig _mysqlconf;
+			bool openDatabase();
+	};
+}
+
+#endif
---
+++ tnode/src/db/mysql/MySQLResult.cpp
@@ -0,0 +1,36 @@
+/*
+ * \file: MySQLResult.cpp
+ * \brief: Created by hushouguo at 17:26:45 Apr 11 2019
+ */
+
+#include "Database.h"
+
+namespace db {
+	MySQLResult::MySQLResult(MYSQL_RES* res) : _res(res) {
+	}
+
+	MySQLResult::~MySQLResult() {
+		this->free();
+	}
+
+	void MySQLResult::free() {
+		mysql_free_result(this->_res);
+	}
+
+	MYSQL_ROW MySQLResult::fetchRow() {
+		return mysql_fetch_row(this->_res);
+	}
+
+	u64 MySQLResult::rowNumber() {
+		return mysql_num_rows(this->_res);
+	}
+
+	u32 MySQLResult::fieldNumber() {
+		return mysql_num_fields(this->_res);
+	}
+	
+	MYSQL_FIELD* MySQLResult::fetchField() {
+		return mysql_fetch_fields(this->_res);
+	}
+}
+
