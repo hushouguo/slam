679e10ecac4441a8f5d7bd36e05f785d4e23847b
hushouguo <kilimajaro@gmail.com>
2019-04-17 09:24:40 +0800

    fix bug

---
+++ tnode/src/message/MessageParser.cpp
@@ -0,0 +1,10 @@
+/*
+ * \file: MessageParser.cpp
+ * \brief: Created by hushouguo at 09:24:04 Apr 17 2019
+ */
+
+#include "tnode.h"
+
+BEGIN_NAMESPACE_TNODE {
+}
+
---
+++ tnode/src/message/MessageParser.h
@@ -0,0 +1,14 @@
+/*
+ * \file: MessageParser.h
+ * \brief: Created by hushouguo at 09:23:36 Apr 17 2019
+ */
+ 
+#ifndef __MESSAGEPARSER_H__
+#define __MESSAGEPARSER_H__
+
+BEGIN_NAMESPACE_TNODE {
+	class MessageParser {
+	};
+}
+
+#endif
