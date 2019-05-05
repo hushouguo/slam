33f390ef291dd811966e69314cbdedfdbf18a573
hushouguo <kilimajaro@gmail.com>
2019-05-05 17:49:51 +0800

    fix bug

--- shard/GatewayServer/src/main.cpp
+++ shard/GatewayServer/src/main.cpp
@@ -15,50 +15,6 @@
 
 using namespace slam;
 
-void install_signal_handler(std::function<void(int sig)> handler) {
-	struct sigaction act;
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = SA_INTERRUPT; //The system call that is interrupted by this signal will not be restarted automatically
-	act.sa_handler = [](int sig) {
-		//fprintf(stderr, "receive signal: %d\n", sig);
-		// Don't call Non reentrant function, just like malloc, free etc, i/o function also cannot call.
-		//if (sig == SIGRTMIN) {		// SIGRTMIN: Wake up thread, nothing to do
-		//	return;	// SIGRTMIN: #define SIGRTMIN        (__libc_current_sigrtmin ())
-		//}
-		switch (sig) {
-			case SIGINT:
-			case SIGTERM:
-			case SIGQUIT: 
-				sConfig.halt = true;
-				sMainProcess.wakeup();
-				break;	// Note: schedule halt
-		
-			case SIGHUP: 
-				sConfig.reload = true; break; // NOTE: reload configure file
-
-			case SIGUSR1:
-			case SIGUSR2:
-				//dump system runtime information
-				//tc_malloc_stats();
-				break;
-			
-			case SIGWINCH: break;	// the window size change, ignore
-			//case SIGALRM: break;	// timer expire
-			
-			default: sConfig.syshalt(sig); break;
-		}
-	};
-
-	sigaction(SIGINT, &act, nullptr);
-	sigaction(SIGTERM, &act, nullptr);
-	sigaction(SIGQUIT, &act, nullptr);
-	sigaction(SIGHUP, &act, nullptr);
-	sigaction(SIGUSR1, &act, nullptr);
-	sigaction(SIGUSR2, &act, nullptr);
-	//sigaction(SIGALRM, &act, nullptr);
-	//sigaction(SIGRTMIN, &act, nullptr);
-}
-
 int main(int argc, char* argv[]) {
 	// Verify that the version of the library that we linked against is
 	// compatible with the version of the headers we compiled against.
